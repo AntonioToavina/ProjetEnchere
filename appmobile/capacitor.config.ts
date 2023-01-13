814df5753d4fe245a5bc5a94655b41670dfc1bbc
Antonio <toavinarakotoarisoa024@gmail.com>
2023-01-13 06:53:07 +0300

    AppMobile

---
+++ appmobile/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.appmobile',
+  appName: 'appmobile',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
