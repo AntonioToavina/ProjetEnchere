a4cf059161338a848b8fd110b6210565b314de4e
Antonio <toavinarakotoarisoa024@gmail.com>
2023-01-18 11:19:00 +0300

    Modif

--- serverApp/src/main/java/com/example/serverapp/Model/User_account.java
+++ serverApp/src/main/java/com/example/serverapp/Model/User_account.java
@@ -1,5 +1,7 @@
 package com.example.serverapp.Model;
 
+import com.example.serverapp.Repository.Repo_Token;
+import com.example.serverapp.Repository.Repo_User;
 import org.hibernate.annotations.DynamicInsert;
 
 import javax.persistence.*;
@@ -69,5 +71,43 @@ public class User_account {
                 ", email='" + email + '\'' +
                 ", password='" + password + '\'' +
                 '}';
+    }
+
+    public void inscription(Repo_User repo_user)throws Exception{
+        try {
+            User_account otherUser=repo_user.checkUser(this.username,this.email,this.password);
+            if(otherUser!=null)
+                throw new Exception("Erreur lors de l'inscription");
+            repo_user.save(this);
+        }catch(Throwable e){
+            throw new Exception("Erreur lors de l'inscription");
+        }
+    }
+
+    public Token login(Repo_User repo_user, Repo_Token repo_token) throws Exception{
+        Token token=new Token();
+        try{
+            User_account user=repo_user.userLogin(this.email,this.password);
+            token.generateToken(user);
+            token.setUser_account(user);
+            repo_token.save(token);
+        }catch(Exception e){
+            throw new Exception("Mot de passe/email incorrect");
+        }
+        return token;
+    }
+
+    public void deconnexion(Repo_Token repo_token,String token)throws Exception{
+        try{
+            Token token_user=new Token();
+            token_user=token_user.check_Expiration(token,repo_token);
+            if(token_user==null)
+                throw new Exception("Error");
+
+
+            repo_token.deleteToken(this.id);
+        }catch(Throwable e){
+            throw new Exception("Error");
+        }
     }
 }
